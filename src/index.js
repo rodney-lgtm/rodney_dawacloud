fbd4dd45d5305758f1ccf2038b1f234c175c7c98
rodney <musyokarrodney@gmail.com>
2026-03-24 14:38:59 +0300

    change

--- src/index.js
+++ src/index.js
@@ -1,17 +1,79 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import axios from "axios";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const MakePaymentComponent = () => {
+    const { product } = useLocation().state || {};
+    let [phone, setPhone] = useState("")
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    let [loading, setLoading] = useState("")
+    let [error, setError] = useState("")
+    let [success, setSuccess] = useState("")
+
+
+    const img_url = "https://bradleylee.alwaysdata.net/static/images/"
+    console.log(product)
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setError("")
+        setSuccess("")
+        setLoading("Please Wait...")
+
+        try {
+            const data = new FormData()
+            data.append("amount", product.product_cost)
+            data.append("phone", phone)
+
+            const response = await axios.post("https://rodfelix.alwaysdata.net/api/mpesa_payment", data)
+            console.log(data)
+            if (response.status === 200) {
+                setLoading("")
+                setSuccess(response.data.message)
+            }
+
+        } catch (error) {
+            setLoading("")
+            setError(error.message)
+
+        }
+    }
+    return (
+        <div className="row justify-content-center mt-3">
+            <h2>LIPA NA MPESA</h2>
+            <div className="col-md-3">
+                <img src={img_url + product.product_image} alt="" className="rounded img-thumbnail" />
+            </div>
+            <div className="col-md-3">
+                <h2 className="text-dark">{product.product_name}</h2>
+                <h5 className="text-primary">{product.product_category}</h5>
+                <p className="text-muted">{product.product_description}</p>
+                <h3 className="text-warning">{product.product_cost}</h3>
+                <hr />
+                <h6 className="text-warning">{loading}</h6>
+                <h6 className="text-danger">{error}</h6>
+                <h6 className="text-success">{success}</h6>
+
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="text"
+                        className="form-control"
+                        readOnly
+                        value={product.product_cost}
+                        placeholder="Enter Amount" />
+                    <br />
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Enter Mpesa Number 2547xxxxxxx"
+                        onChange={(e) => { setPhone(e.target.value) }}
+                        value={phone} />
+                    <button className="btn btn-dark">Pay Now</button>
+
+                </form>
+            </div>
+
+        </div>
+    );
+}
+export default MakePaymentComponent;
